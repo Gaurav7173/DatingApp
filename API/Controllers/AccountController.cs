731adc98bb57a1512be136cce5e0fd0a64b12e84
Gaurav7173 <gaurav.kumar7173.com>
2024-12-30 01:00:57 +0530

    #Creating a User DTO and returning the token

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -6,13 +6,14 @@ using API.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using API.DTOs;
+using API.Interfaces;
 
 namespace API.Controllers;
 
-public class AccountController(DataContext context): BaseApiController
+public class AccountController(DataContext context,ITokenService tokenService): BaseApiController
 {
     [HttpPost("register")]//account/register
-    public async Task<ActionResult<AppUser>>Register(RegisterDto registerDto)
+    public async Task<ActionResult<UserDto>>Register(RegisterDto registerDto)
     {
         if(await UserExists(registerDto.Username)) return BadRequest("Username is taken");
         
@@ -27,11 +28,15 @@ public class AccountController(DataContext context): BaseApiController
         context.Users.Add(user);
         await context.SaveChangesAsync();
 
-        return user;
+        return new UserDto
+        {
+            Username=user.UserName,
+            Token=tokenService.CreateToken(user)
+        };
     }
 
     [HttpPost("login")]
-    public async Task<ActionResult<AppUser>>Login(LoginDto loginDto)
+    public async Task<ActionResult<UserDto>>Login(LoginDto loginDto)
     {
         var user=await context.Users.FirstOrDefaultAsync(x=>x.UserName==loginDto.Username.ToLower());
         if(user==null) return Unauthorized("Invalid username or password");
@@ -44,7 +49,11 @@ public class AccountController(DataContext context): BaseApiController
             if(computedHash[i]!=user.PasswordHash[i]) return Unauthorized("Invalid username or password");
         }
 
-        return user;
+        return new UserDto
+        {
+            Username=user.UserName,
+            Token=tokenService.CreateToken(user)
+        };
         
     }
 
