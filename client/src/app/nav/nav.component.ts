79476c34866677a3bde1ab5c91e3294c39e7a2e0
Gaurav7173 <gaurav.kumar7173.com>
2024-12-31 06:58:40 +0530

    Inject Services into components

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component,inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-nav',
@@ -8,11 +9,20 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
-export class NavComponent {
+export class NavComponent 
+{
+  private accontService= inject(AccountService);
+  loggedIn= false;
   model: any = {};
 
-  login() {
-    console.log(this.model);
+  login() 
+  {
+    this.accontService.login(this.model).subscribe({
+      next:response => {
+        console.log(response);
+        this.loggedIn = true;
+      },
+      error: error => console.log(error)
+    })
   }
-
 }
