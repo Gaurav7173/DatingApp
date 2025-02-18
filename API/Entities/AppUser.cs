ba8ded1248fd69a3d91790ec20753667ff0c2bd2
Gaurav7173 <gaurav.kumar7173.com>
2025-02-18 07:37:21 +0530

    # Extending the user entity

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -5,7 +5,19 @@ public class AppUser
     public int Id { get; set; }
     public required string UserName { get; set; }
 
-    public required byte[] PasswordHash { get; set; }
-    public required byte[] PasswordSalt { get; set; }
+    public byte[] PasswordHash { get; set; }=[];
+    public byte[] PasswordSalt { get; set; }=[];
+    public DateOnly DateOfBirth { get; set; }
+    public required string KnownAs { get; set; }
+    public DateTime created { get; set; } = DateTime.Now;
+    public DateTime LastActive { get; set; } = DateTime.Now;
+    public required string Gender { get; set; }
+    public string? Introduction { get; set; }
+    public string? Interests { get; set; }
+    public string? LookingFor { get; set; }
+    public required string City { get; set; }
+    public required string Country { get; set; }
+    public List<Photo> Photos { get; set; }=[];
+
 
 }
